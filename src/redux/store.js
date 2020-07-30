973aa576c44ca7310a9f9bad63f95029e1ec8f89
Maxim Kozin <maxa.kozin@gmail.com>
2020-07-30 12:19:33 +0300

    uploading hw-7 added selectors and memo

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  // persistStore,
+  // persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+import rootReducer from './phonebook/phonebook-reducer';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  })
+];
+
+// const contactsPersistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter', '_persist']
+// }
+
+const store = configureStore({
+  reducer: {
+    contacts: rootReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+// const persistor = persistStore(store);
+
+export default store;
