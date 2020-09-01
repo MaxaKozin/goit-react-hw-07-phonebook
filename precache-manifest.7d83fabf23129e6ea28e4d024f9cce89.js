9146cb1e7620956813d354f7ae0ae0cf6c262540
Maxim Kozin <maxa.kozin@gmail.com>
2020-09-01 21:12:22 +0300

    Updates

--- precache-manifest.7d83fabf23129e6ea28e4d024f9cce89.js
+++ precache-manifest.7d83fabf23129e6ea28e4d024f9cce89.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69ccb7eb3ede26c24db830219c020a14",
+    "revision": "bab06ed8e9b1a7178d75fa68e37cd000",
     "url": "/goit-react-hw-07-phonebook/index.html"
   },
   {
-    "revision": "1d8d2605e53b63bc4897",
+    "revision": "1425c5650144a20de39e",
     "url": "/goit-react-hw-07-phonebook/static/css/main.48258e5a.chunk.css"
   },
   {
-    "revision": "9bbdc35ebf32dbc20703",
-    "url": "/goit-react-hw-07-phonebook/static/js/2.6c393e18.chunk.js"
+    "revision": "e106ab2b50d8f635fb2a",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.30675b40.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/goit-react-hw-07-phonebook/static/js/2.6c393e18.chunk.js.LICENSE.txt"
+    "url": "/goit-react-hw-07-phonebook/static/js/2.30675b40.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d8d2605e53b63bc4897",
-    "url": "/goit-react-hw-07-phonebook/static/js/main.c110e558.chunk.js"
+    "revision": "1425c5650144a20de39e",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.55e7424f.chunk.js"
   },
   {
     "revision": "e3f60b8770ae85b5a663",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-07-phonebook/precache-manifest.3ae8e787f23ff79e67df384efeb0c723.js"
+  "/goit-react-hw-07-phonebook/precache-manifest.7d83fabf23129e6ea28e4d024f9cce89.js"
 );
 
 self.addEventListener('message', (event) => {
